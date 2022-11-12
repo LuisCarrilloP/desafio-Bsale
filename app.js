4470888894391b1048b3e3e25d3b40fe08849c8c
LuisCarrilloP <jl.carrillopadron@gmail.com>
2022-11-12 11:59:35 -0600

    access control

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const routes = require('./routes/index.routes');
 
 //Ejecutar express
@@ -6,9 +7,16 @@ const app = express()
 
 //Permitir json
 app.use(express.json())
-
+//Acceso
+app.use(cors({
+  origin: "*",
+  credentials: true,
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  header: 'Origin, X-Requested-With, Content-Type, Accept'
+}))
 //Endpoint base
 app.use('/api/v1', routes);
+
 
 //Global error handler
 app.use((err, req, res, next) => {
